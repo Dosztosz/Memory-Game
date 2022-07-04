f34ae0496da01fc85329321a1f819679256bffd1
Dosztosz <dosztosz@int.pl>
2022-07-04 22:52:31 +0200

    Update

---
+++ app.js
@@ -0,0 +1,86 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+const cardsChosen = []
+
+function createBoard() {
+    for(let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        console.log(card, i)
+        gridDisplay.append(card)
+    
+    }
+}
+createBoard()
+function checkMatch() {
+    console.log('check for match!')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+    }
+}
+
+function flipCard() {
+   const cardId = this.getAttribute('data-id')
+   cardsChosen.push(cardArray[cardId].name)
+   console.log('clicked', cardId)
+   console.log(cardsChosen)
+   this.setAttribute('src', cardArray[cardId].img)
+   if(cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500)
+   }
+}
